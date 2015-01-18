f277cbb3eff61ae7fcc927f712582dbed5d8df5d
Geoffrey Vedernikoff <yefim323@gmail.com>
2015-01-18 01:47:43 -0500

    Added spinner

--- client/songerator.js
+++ client/songerator.js
@@ -95,8 +95,11 @@ Template.record.events({
     e.preventDefault();
     if (audioBlob) {
       var songId = e.target.songId.value;
+      console.log('about to read audioBlob');
       BinaryFileReader.read(audioBlob, function(err, fileInfo) {
+        console.log('about to insert');
         Recordings.insert({songId: songId, userId: Meteor.userId(), blob: fileInfo});
+        console.log('about to redirect');
         Router.go('song', {_id: songId});
       });
     }
@@ -108,3 +111,18 @@ Template.record.helpers({
     return Session.get('isRecording');
   }
 });
+
+Meteor.Spinner.options = {
+  lines: 13, // The number of lines to draw
+  length: 10, // The length of each line
+  width: 5, // The line thickness
+  radius: 15, // The radius of the inner circle
+  corners: 0.7, // Corner roundness (0..1)
+  rotate: 0, // The rotation offset
+  direction: 1, // 1: clockwise, -1: counterclockwise
+  color: '#8cd867', // #rgb or #rrggbb
+  speed: 1, // Rounds per second
+  trail: 60, // Afterglow percentage
+  shadow: true, // Whether to render a shadow
+  className: 'spinner', // The CSS class to assign to the spinner
+};
